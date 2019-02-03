eea40ba2d489cad222a581e4034657bc7aa99a3d
Nicolas <nicog.035@gmail.com>
2019-02-03 19:00:51 +0100

    Added code for intent callback

---
+++ action-kodi.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+
+
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+
+class Kodi(object):
+    def __init__(self):
+        # start listening to MQTT
+        self.start_blocking()
+
+    def turn_on_callback(self, hermes, intent_message):
+        # terminate the session first if not continue
+        hermes.publish_end_session(intent_message.session_id, "")
+
+        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
+
+        # if need to speak the execution result by tts
+        hermes.publish_start_session_notification(intent_message.site_id, "J'ai allumé la télé")
+
+    def turn_off_callback(self, hermes, intent_message):
+        # terminate the session first if not continue
+        hermes.publish_end_session(intent_message.session_id, "")
+
+        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
+
+        # if need to speak the execution result by tts
+        hermes.publish_start_session_notification(intent_message.site_id, "J'ai allumé la télé")
+
+    # --> Master callback function, triggered everytime an intent is recognized
+    def master_intent_callback(self, hermes, intent_message):
+        coming_intent = intent_message.intent.intent_name
+	    if coming_intent  == 'TurnOn':
+            self.turn_on_callback(hermes, intent_message)
+
+	    elif coming_intent == 'TurnOff':
+            self.turn_off_callback(hermes, intent_message)
+
+    # --> Register callback function and start MQTT
+    def start_blocking(self):
+        with Hermes(MQTT_ADDR) as h:
+            h.subscribe_intents(self.master_intent_callback).start()
+
+if __name__ == "__main__":
+    Kodi()
